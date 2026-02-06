a9e7a7ebfc242214ff39f612ee352a009df04ee4
AgentFusion <AgentFusion.ai@gmail.com>
2026-02-06 07:47:40 +0330

    making loading models on Init optional; fix android loading bugs(reported on ssome phones

---
+++ assets/Init-Bkx1tlX7.js
@@ -0,0 +1,1 @@
+import{r as t,j as r,T as i}from"./osRouter-DduoGc9m.js";import"./index-DBpTsiB0.js";const o=()=>(t.useLayoutEffect(()=>{},[]),r.jsx(i,{children:"initializing..."}));export{o as Init};
--- assets/Init-Dp8l2lZx.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as r,T as i}from"./osRouter-C6z0YJ-K.js";import"./index-CdvMKzMv.js";const o=()=>(t.useLayoutEffect(()=>{},[]),r.jsx(i,{children:"initializing..."}));export{o as Init};
