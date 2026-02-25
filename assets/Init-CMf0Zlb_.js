3d3f4e67f2b2d4cbbbdf8ead8b206e791360cc56
AgentFusion <AgentFusion.ai@gmail.com>
2026-02-25 18:43:31 +0100

    make background processing optional; fix bugs;improve loading speed;

---
+++ assets/Init-CMf0Zlb_.js
@@ -0,0 +1,1 @@
+import{r as t,j as r,T as i}from"./osRouter-CUwEwPeI.js";import"./index-DcF40xWb.js";const o=()=>(t.useLayoutEffect(()=>{},[]),r.jsx(i,{children:"initializing..."}));export{o as Init};
